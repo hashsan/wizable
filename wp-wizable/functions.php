017e6f7c2ff0947e69bf9b3e929e84b4a8c2c64c
はっしゅさん <149641384+hashsan@users.noreply.github.com>
2024-05-17 15:44:56 +0900

    Create functions.php

---
+++ wp-wizable/functions.php
@@ -0,0 +1,13 @@
+
+function theme_widgets_init() {
+    register_sidebar( array(
+        'name' => __( 'Main Widget Area', 'text_domain' ),
+        'id' => 'main_widget',
+        'description' => __( 'Widgets in this area will be shown on the main content area.', 'text_domain' ),
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '', // Nothing
+        'after_title' => '',  // Nothing
+    ) );
+}
+add_action( 'widgets_init', 'theme_widgets_init' );
